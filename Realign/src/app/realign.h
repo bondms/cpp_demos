f1c5daccd047f0fd216122447b52d151779d6846
Mark Bond <34947848+bondms@users.noreply.github.com>
2019-10-09 21:25:50 +0100

    Remove unnecessary if

--- Realign/src/app/realign.h
+++ Realign/src/app/realign.h
@@ -7,9 +7,6 @@ template<typename Container>
 void realign(Container & container)
 {
     auto r_it_source{ std::find_if_not(container.rbegin(), container.rend(), std::isspace) };
-    if ( r_it_source != container.rbegin())
-    {
-        auto r_it_moved{ std::move(r_it_source, container.rend(), container.rbegin()) };
-        std::fill(r_it_moved, container.rend(), static_cast<typename Container::value_type>(' '));
-    }
+    auto r_it_moved{ std::move(r_it_source, container.rend(), container.rbegin()) };
+    std::fill(r_it_moved, container.rend(), static_cast<typename Container::value_type>(' '));
 }
