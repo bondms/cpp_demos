c6ed8b18dcf75c5799d86d038b4fad1670800d2e
Mark Bond <34947848+bondms@users.noreply.github.com>
2019-12-14 11:22:42 +0000

    In progress

--- Logger/src/tst/LoggerTest.cpp
+++ Logger/src/tst/LoggerTest.cpp
@@ -2,6 +2,8 @@
 
 #include <gtest/gtest.h>
 
+#include <functional>
+
 namespace
 {
     class LoggerTestFixture :
@@ -10,9 +12,30 @@ namespace
     };
 }
 
+template<typename ExceptionType>
+void expect_throw_with_callback(std::function<void()> code, std::function<bool(ExceptionType e)> callback)
+{
+    try
+    {
+        code();
+        ADD_FAILURE() << "Exception not thrown";
+    }
+    catch ( const ExceptionType& e )
+    {
+        EXPECT_TRUE(callback(e));
+    }
+    catch ( ... )
+    {
+        ADD_FAILURE() << "Unexpected exception type thrown";
+    }
+}
+
 TEST_F(LoggerTestFixture, ThrowsOnLogFileCreationFailure)
 {
-    EXPECT_THROW(Logger::Initialise("/invalid/path/to/file.txt"), std::runtime_error);
+    expect_throw_with_callback<std::runtime_error>(
+        []{Logger::Initialise("/invalid/path/to/file.txt");},
+        [](const std::runtime_error& e){ return std::string{"Failed to open log file: /invalid/path/to/file.txt"} == e.what(); }
+        );
 }
 
 TEST_F(LoggerTestFixture, TODO)
