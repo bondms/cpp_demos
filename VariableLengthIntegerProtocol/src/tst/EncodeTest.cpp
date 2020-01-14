a99bd9a482b5a3f139bb7f8e531c848df6cfa3b0
Mark Bond <34947848+bondms@users.noreply.github.com>
2020-01-14 20:38:22 +0000

    Fixing unit test expectations

--- VariableLengthIntegerProtocol/src/tst/EncodeTest.cpp
+++ VariableLengthIntegerProtocol/src/tst/EncodeTest.cpp
@@ -29,13 +29,17 @@ TEST_P(EncodeTestFixture, Simple)
 }
 
 Parameters parameterValues[] = {
-    Parameters(size_t{0}, std::vector<unsigned char>{}),
+    Parameters(size_t{0}, std::vector<unsigned char>{0}),
     Parameters(size_t{1}, std::vector<unsigned char>{1}),
     Parameters(size_t{2}, std::vector<unsigned char>{2}),
-    Parameters(size_t{0xFF}, std::vector<unsigned char>{0xFF}),
-    Parameters(size_t{0x100}, std::vector<unsigned char>{0x1, 0x0}),
-    Parameters(size_t{0x101}, std::vector<unsigned char>{0x1, 0x1}),
-    Parameters(size_t{0xFE0087}, std::vector<unsigned char>{0xFE, 0x00, 0x87})
+    Parameters(size_t{0x7F}, std::vector<unsigned char>{0x7F}),
+    Parameters(size_t{0x80}, std::vector<unsigned char>{0x80, 0x80}),
+    Parameters(size_t{0x81}, std::vector<unsigned char>{0x80, 0x81}),
+    Parameters(size_t{0xFF}, std::vector<unsigned char>{0x80, 0xFF}),
+    Parameters(size_t{0x100}, std::vector<unsigned char>{0x81, 0x00}),
+    Parameters(size_t{0x101}, std::vector<unsigned char>{0x81, 0x01}),
+    Parameters(size_t{0x0200AB}, std::vector<unsigned char>{0xC2, 0x00, 0xAB}),
+    Parameters(size_t{0x1200AB}, std::vector<unsigned char>{0xD2, 0x00, 0xAB})
 };
 
 INSTANTIATE_TEST_CASE_P(EncodeTestInstantiation, EncodeTestFixture, testing::ValuesIn(parameterValues));
