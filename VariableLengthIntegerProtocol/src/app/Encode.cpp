2cf22da6aacee6a125ff04c6a5e33a9d8495a662
Mark Bond <34947848+bondms@users.noreply.github.com>
2020-01-14 21:54:07 +0000

    In progress

--- VariableLengthIntegerProtocol/src/app/Encode.cpp
+++ VariableLengthIntegerProtocol/src/app/Encode.cpp
@@ -14,7 +14,7 @@ namespace
     auto shift_left_byte(T& v)
     {
         constexpr auto bits_in_t{ sizeof(T) * CHAR_BIT };
-        uint8_t result{ v >> (bits_in_t - 8) };
+        auto result{ static_cast<uint8_t>(v >> (bits_in_t - 8)) };
         v = v << 8;
         return result;
     }
@@ -22,6 +22,8 @@ namespace
 
 std::vector<uint8_t> Encode(size_t input)
 {
+    constexpr auto uint8sInInput{ (sizeof(input) * CHAR_BIT) / 8 };
+
     std::vector<uint8_t> result{};
 
     if ( 0 == input )
@@ -31,16 +33,20 @@ std::vector<uint8_t> Encode(size_t input)
     }
 
     uint8_t currentByte{ 0 };
+    auto shifts{ 0U };
 
     while ( (0 == currentByte) && (0 != input) )
     {
         currentByte = shift_left_byte(input);
+        ++shifts;
     }
 
-    while ( (0 != currentByte) || (0 != input) )
+    while ( shifts <= uint8sInInput )
     {
         result.push_back(currentByte);
+
         currentByte = shift_left_byte(input);
+        ++shifts;
     }
 
     return result;
