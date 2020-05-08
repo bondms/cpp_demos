09d57964df0ff4d36c44cb86f07dfa9d2ffb0352
Mark Bond <34947848+bondms@users.noreply.github.com>
2020-05-08 13:18:22 +0100

    Unit test for scope exit

---
+++ Common/scope_exit_test.cpp
@@ -0,0 +1,37 @@
+#include "../app/scope_exit.h"
+
+#include "gtest_helper.h"
+
+namespace
+{
+    class ScopeExitTestFixture :
+        public testing::Test
+    {
+    };
+}
+
+TEST_F(ScopeExitTestFixture, ScopeExitsCleanly)
+{
+    bool b = false;
+    {
+        auto se = make_scope_exit([&](){ b = true; });
+        EXPECT_FALSE(b);
+    }
+    EXPECT_TRUE(b);
+}
+
+TEST_F(ScopeExitTestFixture, ScopeExitsOnException)
+{
+    bool b = false;
+    try
+    {
+        auto se = make_scope_exit([&](){ b = true; });
+        EXPECT_FALSE(b);
+        throw std::runtime_error{"test"};
+    }
+    catch ( const std::exception & )
+    {
+        EXPECT_TRUE(b);
+    }
+    EXPECT_TRUE(b);
+}
