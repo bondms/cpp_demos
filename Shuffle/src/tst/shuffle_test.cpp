ac5ac26da6ae810390f190ca4c7684d63a361775
Mark Bond <34947848+bondms@users.noreply.github.com>
2020-05-28 22:44:59 +0100

    Shuffle

--- Shuffle/src/tst/shuffle_test.cpp
+++ Shuffle/src/tst/shuffle_test.cpp
@@ -12,7 +12,7 @@ namespace
     };
 } // namespace
 
-TEST_F(ShuffleTestFixture, Simple)
+TEST_F(ShuffleTestFixture, Simple_Quick)
 {
     std::set<int> s{};
     Shuffle::Simple<int> simple(9);
@@ -26,13 +26,41 @@ TEST_F(ShuffleTestFixture, Simple)
     }
 }
 
-TEST_F(ShuffleTestFixture, LowMem)
+TEST_F(ShuffleTestFixture, LowMem_Quick)
 {
     std::set<int> s{};
     Shuffle::LowMem<int> lowmem(9);
     for(auto i = 0 ; i < 25 ; ++i)
     {
         if ( 0 == i % 10 )
+        {
+            s.clear();
+        }
+        EXPECT_TRUE(s.insert(lowmem()).second);
+    }
+}
+
+TEST_F(ShuffleTestFixture, Simple_Slow)
+{
+    std::set<int> s{};
+    Shuffle::Simple<int> simple(99'999);
+    for(auto i = 0 ; i < 250'000 ; ++i)
+    {
+        if ( 0 == i % 100'000 )
+        {
+            s.clear();
+        }
+        EXPECT_TRUE(s.insert(simple()).second);
+    }
+}
+
+TEST_F(ShuffleTestFixture, LowMem_Slow)
+{
+    std::set<int> s{};
+    Shuffle::LowMem<int> lowmem(99'999);
+    for(auto i = 0 ; i < 250'000 ; ++i)
+    {
+        if ( 0 == i % 100'000 )
         {
             s.clear();
         }
