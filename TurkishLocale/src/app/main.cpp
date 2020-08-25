f410e0b82a159659d2482f82a7df68a76c429377
Mark Bond <34947848+bondms@users.noreply.github.com>
2020-08-25 11:46:50 +0100

    Demonstrate Turkish locale

--- TurkishLocale/src/app/main.cpp
+++ TurkishLocale/src/app/main.cpp
@@ -2,21 +2,38 @@
 #include <iostream>
 #include <locale>
 
+namespace
+{
+    constexpr auto dotted_lowercase{ 'i' };
+    // constexpr auto dotted_uppercase{ '\xc4\xb0' };
+    // constexpr auto dotless_lowercase{ '\xc4\xb1' };
+    constexpr auto dotless_uppercase{ 'I' };
+
+    constexpr auto dotted_lowercase_w{ L'i' };
+    constexpr auto dotted_uppercase_w{ L'\u0130' };
+    constexpr auto dotless_lowercase_w{ L'\u0131' };
+    constexpr auto dotless_uppercase_w{ L'I' };
+}
+
 int main()
 {
     try
     {
-        const auto from{ 'i' };
-        std::cout << "From: " << from << std::endl;
-
+        const auto from{ dotless_lowercase_w };
         const auto to{ std::toupper(from, std::locale("tr_TR.utf8")) };
-        std::cout << "To: " << to << std::endl;
+        if (dotless_uppercase_w != to)
+        {
+            throw std::runtime_error{ "Unexpected result" };
+        }
 
+        std::cout << "Success" << std::endl;
         return EXIT_SUCCESS;
     }
     catch(const std::exception& e)
     {
         std::cerr << "Exception: " << e.what() << std::endl;
     }
+
+    std::cout << "Failure" << std::endl;
     return EXIT_FAILURE;
 }
