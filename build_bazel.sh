4fc4d9a8f9c5e41912458c736299558dfec7a4e9
Mark Bond <34947848+bondms@users.noreply.github.com>
2021-02-14 22:55:46 +0000

    Separate Makefile and Bazel CI

--- build_bazel.sh
+++ build_bazel.sh
@@ -9,13 +9,14 @@ set -u
 HERE="$(readlink -f "$(dirname "$0")")"
 [[ -d "${HERE}" ]] || exit $?
 
-find "${HERE}" -maxdepth 2 -mindepth 2 -name Makefile -type f -print0 | bash -c "
+find "${HERE}" -maxdepth 2 -mindepth 2 -name WORKSPACE -type f -print0 | bash -c "
     while read -r -d $'\0' F
     do
-      echo Processing Makefile \"\$F\"
+      echo Processing WORKSPACE \"\$F\"
       DIR=\$(dirname \"\$F\")
       pushd \"\${DIR}\" > /dev/null || exit \$?
       pwd
-      make || exit \$?
+      bazel build ... || exit \$?
+      bazel test ... || exit \$?
       popd > /dev/null || exit \$?
     done" || exit $?
