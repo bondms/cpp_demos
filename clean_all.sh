c33328514d459a3ae0e4f5cb6964dc47c299cd21
Mark Bond <34947848+bondms@users.noreply.github.com>
2021-02-17 19:55:23 +0000

    Clean up thirdparty too

--- clean_all.sh
+++ clean_all.sh
@@ -10,23 +10,28 @@ HERE="$(readlink -f "$(dirname "$0")")"
 [[ -d "${HERE}" ]] || exit $?
 
 find "${HERE}" -maxdepth 2 -mindepth 2 -name Makefile -type f -print0 | bash -c "
-    while read -r -d $'\0' F
-    do
-      echo Processing Makefile \"\$F\"
-      DIR=\$(dirname \"\$F\")
-      pushd \"\${DIR}\" > /dev/null || exit \$?
-      pwd
-      make clean || exit \$?
-      popd > /dev/null || exit \$?
-    done"
+  while read -r -d $'\0' F
+  do
+    echo Processing Makefile \"\$F\"
+    DIR=\$(dirname \"\$F\")
+    pushd \"\${DIR}\" > /dev/null || exit \$?
+    pwd
+    make clean || exit \$?
+    popd > /dev/null || exit \$?
+  done" || exit $?
 
 find "${HERE}" -maxdepth 2 -mindepth 2 -name WORKSPACE -type f -print0 | bash -c "
-    while read -r -d $'\0' F
-    do
-      echo Processing WORKSPACE \"\$F\"
-      DIR=\$(dirname \"\$F\")
-      pushd \"\${DIR}\" > /dev/null || exit \$?
-      pwd
-      bazel clean --expunge || exit \$?
-      popd > /dev/null || exit \$?
-    done"
+  while read -r -d $'\0' F
+  do
+    echo Processing WORKSPACE \"\$F\"
+    DIR=\$(dirname \"\$F\")
+    pushd \"\${DIR}\" > /dev/null || exit \$?
+    pwd
+    bazel clean --expunge || exit \$?
+    popd > /dev/null || exit \$?
+  done" || exit $?
+
+if [[ -e "${HERE}/thirdparty" ]]
+then
+  find "${HERE}/thirdparty" -delete || exit $?
+fi
