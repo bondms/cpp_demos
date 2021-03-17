250b9eec3f588c4f85d01c56043a55e368d4e20d
Mark Bond <34947848+bondms@users.noreply.github.com>
2021-03-17 22:35:26 +0000

    Add a null test so that "bazel test" doesn't fail

---
+++ TurkishLocale/tst/null_test.cpp
@@ -0,0 +1,8 @@
+#include <gmock/gmock.h>
+
+TEST(NullTest, NullTest)
+{
+    // There are no real tests for this project.
+    // But Bazel requires at least one test in order for `bazel test ...` to succeed.
+    EXPECT_TRUE(true);
+}
