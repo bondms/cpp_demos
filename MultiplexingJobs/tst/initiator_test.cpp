a72717e2bea4079dc2d87d2ba58312c682141bd3
Mark Bond <34947848+bondms@users.noreply.github.com>
2021-07-25 20:53:30 +0100

    Multiplexing jobs

--- MultiplexingJobs/tst/initiator_test.cpp
+++ MultiplexingJobs/tst/initiator_test.cpp
@@ -4,12 +4,18 @@
 
 #include "lib/initiator.h"
 
-TEST(InitiatorTest, NoJobs) {
+TEST(InitiatorTest, EndsOnQuit) {
     Sync<std::string> sync{};
 
-    // auto initiateFunc = [&](const std::string&) -> void {
-    //     throw std::runtime_error{ "Unexpected call to initiatFunc" };
-    // };
+    auto initiateFunc = [&](const std::string&) -> void {
+        throw std::runtime_error{ "Unexpected call to initiatFunc" };
+    };
 
-    // Initiator<std::string, int> initiator{sync, initiateFunc};
+    Initiator<std::string, int> initiator{sync, initiateFunc};
+
+    {
+        std::unique_lock<std::mutex> lock{ sync.mutex_ };
+        sync.quit_ = true;
+    }
+    sync.condition_variable_.notify_one();
 }
