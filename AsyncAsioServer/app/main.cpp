91708be93898e3b5078c181eb4553f1d72a10c37
Mark Bond <34947848+bondms@users.noreply.github.com>
2021-07-31 16:07:18 +0100

    Copyright notice

--- AsyncAsioServer/app/main.cpp
+++ AsyncAsioServer/app/main.cpp
@@ -1,12 +1,10 @@
+// Copyright 2021 Mark Bond
 //
-// server.cpp
-// ~~~~~~~~~~
-//
+// Originally based on ASIO tutorial "An asynchronous TCP daytime server"
 // Copyright (c) 2003-2021 Christopher M. Kohlhoff (chris at kohlhoff dot com)
 //
 // Distributed under the Boost Software License, Version 1.0. (See accompanying
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
-//
 
 #include <ctime>
 #include <iostream>
