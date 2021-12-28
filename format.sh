930017b1999600586def5dc72371931f2c7d749d
Mark Bond <34947848+bondms@users.noreply.github.com>
2021-12-28 18:14:22 +0000

    Format cc files too

--- format.sh
+++ format.sh
@@ -9,4 +9,6 @@ set -u
 HERE="$(readlink -f "$(dirname "$0")")"
 [[ -d "${HERE}" ]] || exit $?
 
-find "${HERE}/." \( -name "*.cpp" -o -name "*.h" \) -print0 | xargs --null --max-args=1 clang-format -i -- || exit $?
+find "${HERE}/." \
+    \( -name "*.cpp" -o -name "*.cc" -o -name "*.h" \) \
+    -print0 | xargs --null --max-args=1 clang-format -i -- || exit $?
