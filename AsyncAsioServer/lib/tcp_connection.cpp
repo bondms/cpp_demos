a96d986daa767b7f6750b34a0d51ae8d68b1e024
Mark Bond <34947848+bondms@users.noreply.github.com>
2021-12-29 18:58:34 +0000

    Split into units

---
+++ AsyncAsioServer/lib/tcp_connection.cpp
@@ -0,0 +1,46 @@
+// Copyright 2021 Mark Bond
+//
+// Originally based on ASIO tutorial "An asynchronous TCP daytime server"
+// Copyright (c) 2003-2021 Christopher M. Kohlhoff (chris at kohlhoff dot com)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#include "AsyncAsioServer/lib/tcp_connection.h"
+
+#include <ctime>
+#include <iostream>
+
+void TcpConnection::handle_write(const asio::error_code &error,
+                                 size_t bytes_transferred) {
+  if (!error) {
+    std::cout << "Sent bytes: " << bytes_transferred << std::endl;
+    return;
+  }
+
+  std::cerr << "Error sending, value: " << error.value() << std::endl;
+}
+
+TcpConnection::TcpConnection(PrivateConstruction, asio::io_context &io_context)
+    : socket_{io_context} {}
+
+TcpConnection::SharedPointer
+TcpConnection::create(asio::io_context &io_context) {
+  return std::make_shared<TcpConnection>(PrivateConstruction{}, io_context);
+}
+
+asio::ip::tcp::socket &TcpConnection::socket() { return socket_; }
+
+void TcpConnection::start() {
+  const time_t now{std::time(0)};
+  message_ = std::ctime(&now);
+
+  std::cout << "Sending message: " << message_ << std::flush;
+
+  asio::async_write(
+      socket_, asio::buffer(message_),
+      [shared_this = shared_from_this()](const asio::error_code &error,
+                                         size_t bytes_transferred) {
+        shared_this->handle_write(error, bytes_transferred);
+      });
+}
---
+++ AsyncAsioServer/lib/tcp_server.cpp
@@ -0,0 +1,57 @@
+// Copyright 2021 Mark Bond
+//
+// Originally based on ASIO tutorial "An asynchronous TCP daytime server"
+// Copyright (c) 2003-2021 Christopher M. Kohlhoff (chris at kohlhoff dot com)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#include "AsyncAsioServer/lib/tcp_server.h"
+
+#include <iostream>
+
+void TcpServer::start_accept() {
+  TcpConnection::SharedPointer new_connection{
+      TcpConnection::create(io_context_)};
+
+  std::lock_guard<std::mutex> lock{mutex_};
+  acceptor_.async_accept(new_connection->socket(),
+                         [this, new_connection](const asio::error_code &error) {
+                           this->handle_accept(new_connection, error);
+                         });
+}
+
+void TcpServer::handle_accept(TcpConnection::SharedPointer new_connection,
+                              const asio::error_code &error) {
+  if (asio::error::operation_aborted == error.value()) {
+    std::cout << "Aborting accept for new connection." << std::endl;
+    // Return without starting a fresh accept to allow clean shutdown.
+    return;
+  }
+
+  if (!error) {
+    std::cout << "Connection accepted." << std::endl;
+    new_connection->start();
+  } else {
+    std::cerr << "Error accepting connection, value: " << error.value()
+              << std::endl;
+  }
+
+  std::cout << "Accept next connection." << std::endl;
+  start_accept();
+}
+
+TcpServer::TcpServer(asio::io_context &io_context)
+    : io_context_{io_context}, acceptor_{io_context,
+                                         asio::ip::tcp::endpoint{
+                                             asio::ip::tcp::v4(), port_}} {
+  std::cout << "Listening on port: " << port_ << std::endl;
+
+  std::cout << "Accept first connection." << std::endl;
+  start_accept();
+}
+
+void TcpServer::shutdown() {
+  std::lock_guard<std::mutex> lock{mutex_};
+  acceptor_.close();
+}
