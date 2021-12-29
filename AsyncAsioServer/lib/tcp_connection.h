e1ac027a96ea834c01aab648c89bc596b7b96829
Mark Bond <34947848+bondms@users.noreply.github.com>
2021-12-29 18:40:01 +0000

    Splitting into units

---
+++ AsyncAsioServer/lib/tcp_connection.h
@@ -0,0 +1,34 @@
+// Copyright 2021 Mark Bond
+//
+// Originally based on ASIO tutorial "An asynchronous TCP daytime server"
+// Copyright (c) 2003-2021 Christopher M. Kohlhoff (chris at kohlhoff dot com)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#pragma once
+
+#include <memory>
+#include <string>
+
+#include <asio.hpp>
+
+class TcpConnection : public std::enable_shared_from_this<TcpConnection> {
+  struct PrivateConstruction {};
+
+  asio::ip::tcp::socket socket_;
+  std::string message_{};
+
+  void handle_write(const asio::error_code &error, size_t bytes_transferred);
+
+public:
+  TcpConnection(PrivateConstruction, asio::io_context &io_context);
+
+  using SharedPointer = std::shared_ptr<TcpConnection>;
+
+  static SharedPointer create(asio::io_context &io_context);
+
+  asio::ip::tcp::socket &socket();
+
+  void start();
+};
---
+++ AsyncAsioServer/lib/tcp_server.h
@@ -0,0 +1,33 @@
+// Copyright 2021 Mark Bond
+//
+// Originally based on ASIO tutorial "An asynchronous TCP daytime server"
+// Copyright (c) 2003-2021 Christopher M. Kohlhoff (chris at kohlhoff dot com)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#pragma once
+
+#include <mutex>
+
+#include <AsyncAsioServer/lib/tcp_connection.h>
+
+#include <asio.hpp>
+
+class TcpServer {
+  constexpr static asio::ip::port_type port_{8013};
+
+  asio::io_context &io_context_;
+  asio::ip::tcp::acceptor acceptor_;
+  std::mutex mutex_{};
+
+  void start_accept();
+
+  void handle_accept(TcpConnection::SharedPointer new_connection,
+                     const asio::error_code &error);
+
+public:
+  explicit TcpServer(asio::io_context &io_context);
+
+  void shutdown();
+};
