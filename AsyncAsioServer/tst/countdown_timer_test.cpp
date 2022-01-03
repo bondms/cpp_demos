e1540266128eea3e6b58729048d6181118b67c8e
Mark Bond <34947848+bondms@users.noreply.github.com>
2022-01-03 15:48:00 +0000

    Unit test CountdownTimer

--- AsyncAsioServer/tst/countdown_timer_test.cpp
+++ AsyncAsioServer/tst/countdown_timer_test.cpp
@@ -4,6 +4,8 @@
 
 #include <gmock/gmock.h>
 
+using std::chrono_literals::operator""ms;
+
 namespace {
 
 class CountdownTimerTestFixture : public testing::Test {};
@@ -11,5 +13,14 @@ class CountdownTimerTestFixture : public testing::Test {};
 } // namespace
 
 TEST_F(CountdownTimerTestFixture, simple) {
-  ADD_FAILURE() << "TODO(MarkBond): ...";
+  asio::io_context io_context{};
+
+  std::vector<int> counts{};
+
+  CountdownTimer timer{io_context, 1ms};
+  timer.initiate([&](int value) { counts.push_back(value); });
+
+  io_context.run();
+
+  EXPECT_THAT(counts, testing::ElementsAre(9, 8, 7, 6, 5, 4, 3, 2, 1, 0));
 }
