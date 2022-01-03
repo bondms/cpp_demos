445ffd7896d8647c6850152c0c9dc7f02eb87b5d
Mark Bond <34947848+bondms@users.noreply.github.com>
2022-01-03 15:29:30 +0000

    Unit test CountdownTimer

---
+++ AsyncAsioServer/tst/countdown_timer_test.cpp
@@ -0,0 +1,15 @@
+// Copyright 2021 Mark Bond
+
+#include "AsyncAsioServer/lib/countdown_timer.h"
+
+#include <gmock/gmock.h>
+
+namespace {
+
+class CountdownTimerTestFixture : public testing::Test {};
+
+} // namespace
+
+TEST_F(CountdownTimerTestFixture, simple) {
+  ADD_FAILURE() << "TODO(MarkBond): ...";
+}
