ae6fd5b0df43ad57ed3afcb58a30392e9a9ef5eb
Mark Bond <34947848+bondms@users.noreply.github.com>
2022-01-06 21:38:18 +0000

    Testing server

---
+++ AsyncAsioServer/tst/tcp_server_test.cpp
@@ -0,0 +1,34 @@
+// Copyright 2021 Mark Bond
+
+#include "AsyncAsioServer/lib/tcp_server.h"
+
+#include <thread>
+
+#include <gmock/gmock.h>
+
+namespace {
+
+class TcpServerTestFixture : public testing::Test {};
+
+constexpr static asio::ip::port_type port{8013};
+
+} // namespace
+
+TEST_F(TcpServerTestFixture, Simple) {
+  asio::io_context io_context{};
+
+  TcpServer server{io_context};
+
+  asio::ip::tcp::socket client_socket{io_context};
+  client_socket.open(asio::ip::tcp::v4());
+  client_socket.async_connect(
+      asio::ip::tcp::endpoint{asio::ip::tcp::v4(), 8013},
+      [](const asio::error_code & /*error*/) {});
+
+  std::vector<char> data(10, '\0');
+  client_socket.async_read_some(asio::buffer(data, data.size()),
+                                [](const asio::error_code & /*error*/,
+                                   std::size_t /*bytes_transferred*/) {});
+
+  io_context.run();
+}
