b389a7a4a770f7805d2c812621d941ac0725f9cf
Mark Bond <34947848+bondms@users.noreply.github.com>
2022-01-06 21:35:49 +0000

    Testing connection

--- AsyncAsioServer/tst/tcp_connection_test.cpp
+++ AsyncAsioServer/tst/tcp_connection_test.cpp
@@ -2,28 +2,48 @@
 
 #include "AsyncAsioServer/lib/tcp_connection.h"
 
+#include <thread>
+
 #include <gmock/gmock.h>
 
 namespace {
 
 class TcpConnectionTestFixture : public testing::Test {};
 
+constexpr static asio::ip::port_type port{8013};
+
 } // namespace
 
 TEST_F(TcpConnectionTestFixture, Simple) {
-  asio::io_context io_context{};
+  // std::thread server = []{
+  //   asio::io_context io_context{};
+  //   auto connection{TcpConnection::create(io_context)};
 
-  auto connection{TcpConnection::create(io_context)};
-  connection->socket().open(asio::ip::tcp::v4());
-  connection->socket().bind(asio::ip::tcp::endpoint{asio::ip::tcp::v4(), 8013});
-  connection->socket().wait(asio::ip::tcp::socket::wait_write);
+  //   io_context.run();
+  // };
 
-  asio::ip::tcp::socket client_socket{io_context};
-  client_socket.open(asio::ip::tcp::v4());
-  // client_socket.connect(asio::ip::tcp::endpoint{asio::ip::tcp::v4(), 8013});
+  /////
 
-  std::vector<char> data(10, '\0');
-  client_socket.async_read_some(asio::buffer(data, data.size()),
-                                [](const asio::error_code & /*error*/,
-                                   std::size_t /*bytes_transferred*/) {});
+  // asio::io_context io_context{};
+
+  // auto connection{TcpConnection::create(io_context)};
+
+  // asio::ip::tcp::acceptor acceptor{
+  //     io_context, asio::ip::tcp::endpoint{asio::ip::tcp::v4(), port}};
+
+  // acceptor_.async_accept(connection->socket(),
+  //                        [](const asio::error_code &error) {
+  //                          xxx;
+  //                        });
+
+  // asio::ip::tcp::socket client_socket{io_context};
+  // client_socket.open(asio::ip::tcp::v4());
+  // client_socket.async_connect(
+  //     asio::ip::tcp::endpoint{asio::ip::tcp::v4(), 8013},
+  //     [](const asio::error_code & /*error*/) {});
+
+  // std::vector<char> data(10, '\0');
+  // client_socket.async_read_some(asio::buffer(data, data.size()),
+  //                               [](const asio::error_code & /*error*/,
+  //                                  std::size_t /*bytes_transferred*/) {});
 }
