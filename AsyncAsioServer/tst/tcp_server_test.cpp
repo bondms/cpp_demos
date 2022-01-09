f5b124ef669cdb16f9663f1c1117423952445cff
Mark Bond <34947848+bondms@users.noreply.github.com>
2022-01-09 14:39:18 +0000

    Test multiple concurrent client connections

--- AsyncAsioServer/tst/tcp_server_test.cpp
+++ AsyncAsioServer/tst/tcp_server_test.cpp
@@ -60,6 +60,75 @@ TEST_F(TcpServerTestFixture, Simple) {
   EXPECT_EQ(expected, std::string(buffer.begin(), it));
 }
 
+TEST_F(TcpServerTestFixture, MultipleClients) {
+  asio::io_context io_context{};
+
+  TcpServer server{io_context, 3, 1ms};
+
+  std::array client_sockets{asio::ip::tcp::socket{io_context},
+                            asio::ip::tcp::socket{io_context},
+                            asio::ip::tcp::socket{io_context}};
+
+  for (auto &client_socket : client_sockets) {
+    client_socket.async_connect(
+        asio::ip::tcp::endpoint{asio::ip::address::from_string("127.0.0.1"),
+                                TcpServer::port},
+        [](const asio::error_code &error) {
+          if (error) {
+            ADD_FAILURE() << "Error (" << error.value()
+                          << "): " << error.message();
+          }
+        });
+  }
+
+  constexpr auto &expected{"2\n1\n0\n"};
+
+  struct Buffer {
+    std::array<char, std::size(expected) + 1> data{};
+    decltype(data)::iterator it{data.begin()};
+  };
+  std::array<Buffer, client_sockets.size()> buffers{};
+
+  auto remaining_clients{client_sockets.size()};
+
+  std::function<void(std::size_t)> async_read_from_client{[&](std::size_t i) {
+    const auto remaining_size{static_cast<std::size_t>(
+        std::distance(buffers[i].it, buffers[i].data.end()))};
+    client_sockets[i].async_read_some(
+        asio::buffer(buffers[i].it, remaining_size),
+        [&, i, remaining_size](const asio::error_code &error,
+                               std::size_t bytes_transferred) {
+          if (error) {
+            if (asio::error::misc_errors::eof != error.value()) {
+              ADD_FAILURE()
+                  << "Error (" << error.value() << "): " << error.message();
+            }
+            if (0 == --remaining_clients) {
+              server.shutdown();
+            }
+            return;
+          }
+          EXPECT_GT(bytes_transferred, 0);
+          EXPECT_LE(bytes_transferred, remaining_size);
+          buffers[i].it += bytes_transferred;
+          async_read_from_client(i);
+        });
+  }};
+
+  for (std::size_t i = 0; i < client_sockets.size(); ++i) {
+    async_read_from_client(i);
+  }
+
+  io_context.run();
+
+  for (const auto &buffer : buffers) {
+    EXPECT_EQ(expected,
+              std::string(buffer.data.cbegin(),
+                          static_cast<decltype(buffer.data)::const_iterator>(
+                              buffer.it)));
+  }
+}
+
 TEST_F(TcpServerTestFixture, AbortAfterFirst) {
   asio::io_context io_context{};
 
@@ -109,7 +178,6 @@ TEST_F(TcpServerTestFixture, AbortAfterFirst) {
 // TODO(MarkBond):
 // * Introduce a slow test which uses a smaller start_from but a reasonable
 // interval and checks the time is at least as long as expected.
-// * Test handling of multiple concucurrent clients.
 // * Test with a large start_from. Generate the expected output with a helper
 // function.
 // * Test re-connect after disconnect.
