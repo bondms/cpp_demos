b9f6023a60f3f415483284f71aa887dddcca63f3
Mark Bond <34947848+bondms@users.noreply.github.com>
2023-01-28 15:57:02 +0000

    Linting

--- build.sh
+++ build.sh
@@ -7,8 +7,8 @@ set -o pipefail
 set -u
 
 HERE="$(readlink -f "$(dirname "$0")")"
-[[ -d "${HERE}" ]] || exit $?
+[[ -d "${HERE}" ]] || exit 1
 
-pushd "${HERE}/." || exit $?
-bazel query //... | xargs bazel test --test_output=errors -- || exit $?
-popd || exit $?
+pushd "${HERE}/." || exit 1
+bazel query //... | xargs bazel test --test_output=errors -- || exit 1
+popd || exit 1
--- format.sh
+++ format.sh
@@ -7,8 +7,8 @@ set -o pipefail
 set -u
 
 HERE="$(readlink -f "$(dirname "$0")")"
-[[ -d "${HERE}" ]] || exit $?
+[[ -d "${HERE}" ]] || exit 1
 
 find "${HERE}/." \
     \( -name "*.cpp" -o -name "*.cc" -o -name "*.h" \) \
-    -print0 | xargs --null clang-format -i -- || exit $?
+    -print0 | xargs --null clang-format -i -- || exit 1
