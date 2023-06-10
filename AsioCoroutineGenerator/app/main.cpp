93622827c84e27d70d4827e5eb7131b5965ad783
Mark Bond <34947848+bondms@users.noreply.github.com>
2023-06-10 22:38:23 +0100

    Re-order headers

--- AsioCoroutineGenerator/app/main.cpp
+++ AsioCoroutineGenerator/app/main.cpp
@@ -3,12 +3,12 @@
 // Distributed under the Boost Software License, Version 1.0. (See accompanying
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
-#include <asio.hpp>
-#include <asio/experimental/coro.hpp>
-
 #include <cstdlib>
 #include <iostream>
 #include <random>
+
+#include <asio.hpp>
+#include <asio/experimental/coro.hpp>
 
 template <typename T>
 asio::experimental::coro<T> randomGenerator(asio::io_context &, T min, T max) {
