ff90b05312dc6b8cd905bcf9d187c3683a7afaa4
Mark Bond <34947848+bondms@users.noreply.github.com>
2023-06-13 22:10:04 +0100

    Remove unnecessary licensing

--- AsioCoroutines/lib/oddifier.h
+++ AsioCoroutines/lib/oddifier.h
@@ -1,7 +1,4 @@
 // Copyright 2023 Mark Bond
-//
-// Distributed under the Boost Software License, Version 1.0. (See accompanying
-// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
 #pragma once
 
