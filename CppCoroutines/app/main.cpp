b92a7396e060fe4bf152ee81a7c32866b62480a7
Mark Bond <34947848+bondms@users.noreply.github.com>
2023-07-15 11:46:24 +0100

    CppCoroutines

--- CppCoroutines/app/main.cpp
+++ CppCoroutines/app/main.cpp
@@ -1,18 +1,45 @@
 // Copyright 2023 Mark Bond
 
 #include <chrono>
+#include <coroutine>
 #include <cstdlib>
 #include <iostream>
+#include <thread>
 
 #include "CppCoroutines/lib/task.h"
 
 using std::chrono_literals::operator""s;
 
+namespace
+{
+  void cpu_work()
+  {
+    std::cout << "Starting CPU work\n";
+    std::this_thread::sleep_for(5s);
+    std::cout << "Completed CPU work\n";
+  }
+
+  Coro gpu_work()
+  {
+    std::cout << "Starting GPU work\n";
+    auto startTime = std::chrono::steady_clock::now();
+    while (std::chrono::steady_clock::now() < startTime + 5s) {
+      co_await std::suspend_always{};
+    }
+    std::cout << "Completed GPU work\n";
+  }
+} // namespace
+
 int main() {
   std::cout << "Starting..." << std::endl;
 
   try {
-    // TODO(MarkBond): ...
+    auto coro = gpu_work();
+    cpu_work();
+
+    while (!coro.done()) {
+      coro.resume();
+    }
 
     std::cout << "Clean shutdown." << std::endl;
 
--- CppCoroutines/lib/task.h
+++ CppCoroutines/lib/task.h
@@ -2,4 +2,32 @@
 
 #pragma once
 
-// TODO(MarkBond): ...
+#include <coroutine>
+
+struct Coro
+{
+  struct promise_type
+  {
+    std::suspend_always initial_suspend() noexcept {
+      return {};
+    }
+
+    std::suspend_always final_suspend() noexcept {
+      return {};
+    }
+
+    Coro get_return_object() {
+      return std::coroutine_handle<promise_type>::from_promise(*this);
+    }
+
+    void return_void() {}
+
+    void unhandled_exception() {
+      std::terminate();
+    }
+  };
+
+  Coro(std::coroutine_handle<promise_type> handle): handle_{ handle } {}
+
+  std::coroutine_handle<promise_type> handle_;
+};
